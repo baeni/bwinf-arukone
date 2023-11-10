a5fdf575bd3d18348c9b539d094e6036981248ac
Benjamin Saalfeld <benjamin.saalfeld@adesso.de>
2023-11-10 13:04:37 +0100

    Add ILogger<ArukoneService>; Implement usage of ArukoneService (Replacing ArukoneHelper); Adjustments and formatting

--- Arukone.Input/Program.cs
+++ Arukone.Input/Program.cs
@@ -1,39 +1,57 @@
 ﻿using Arukone.Logic;
 using Arukone.Logic.Models;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System.Diagnostics;
 
 namespace Arukone.Input
 {
     internal class Program
     {
         private const int BoardsToGenerate = 3;
+
         private const string OutputDirPath = "./output";
+        private const string OutputFileName = "board{0}.txt";
+
+        private const string LogsDirPath = "./logs";
+        private const string LogsFileName = "log-{0}.json";
+
+        private static ArukoneService _arukoneService = default!;
+        private static ILogger<ArukoneService> _logger = default!;
 
         static async Task Main(string[] args)
         {
-            var n = ReadInput();
+            ConfigureLogger();
+            _arukoneService = new ArukoneService(_logger);
 
-            Console.WriteLine("Spielfelder werden generiert... Dieser Vorgang kann gegebenenfalls einige Minuten dauern.");
+            var boardSize = ReadInput();
+
+            AnnounceStart(boardSize);
+
+            #region Generating multiple ArukoneBoards in specified size.
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
 
             var definitions = new List<ArukoneBoardDefinition>();
             for (int i = 0; i < BoardsToGenerate; i++)
             {
-                var definition = new ArukoneBoardDefinition(n);
+                var definition = new ArukoneBoardDefinition(boardSize);
                 definitions.Add(definition);
             }
-            var boards = await ArukoneHelper.GenerateManyBoardsAsync(definitions.ToArray());
+            var boards = await _arukoneService.GenerateManyBoardsAsync(definitions.ToArray());
 
-            Console.Clear();
-            Console.WriteLine($"""
-                Es wurden {boards.Length} Spielfelder mit einer Größe von {n} erstellt.
-                Die Ergebnisse kannst du unter {OutputDirPath} einsehen.
-                """);
+            stopwatch.Stop();
+            #endregion
 
+            #region Outputting results on disk.
             for (int i = 0; i < boards.Length; i++)
             {
-                var serializedBoard = ArukoneHelper.SerializeBoard(boards[i]);
+                var serializedBoard = _arukoneService.SerializeBoard(boards[i]);
                 await SaveBoardToDiskAsync(serializedBoard, i);
-                PrintBoard(serializedBoard);
             }
+            #endregion
+
+            AnnounceEnd(boards.Length, boardSize, stopwatch.ElapsedMilliseconds);
         }
 
         private static int ReadInput()
@@ -42,7 +60,7 @@ namespace Arukone.Input
             do
             {
                 Console.Clear();
-                Console.Write($"Bestimme die Größe des Spielfeldes (>= {MagicNumbers.MinBoardSize}): ");
+                Console.Write(string.Format(Messages.SpecifyBoardSize, MagicNumbers.MinBoardSize));
             }
             while (!int.TryParse(Console.ReadLine(), out n) || n < MagicNumbers.MinBoardSize);
 
@@ -56,15 +74,56 @@ namespace Arukone.Input
                 Directory.CreateDirectory(OutputDirPath);
             }
 
-            var path = Path.Combine(OutputDirPath, $"board{iteration}.txt");
+            var path = Path.Combine(OutputDirPath, string.Format(OutputFileName, iteration));
             await File.WriteAllTextAsync(path, serializedBoard);
         }
 
-        private static void PrintBoard(string serializedBoard)
+        private static void AnnounceStart(int boardSize)
         {
-            Console.WriteLine();
-            Console.Write(serializedBoard);
-            Console.WriteLine();
+            Console.Clear();
+            Console.WriteLine(string.Format(Messages.StartedBoardGeneration, BoardsToGenerate, boardSize));
+        }
+
+        private static void AnnounceEnd(int boardsAmount, int boardSize, long timeElapsed)
+        {
+            var fullOutputpath = Path.GetFullPath(OutputDirPath);
+            var fullLogsPath = Path.GetFullPath(LogsDirPath);
+
+            Console.Clear();
+            Console.Write(string.Format(Messages.FinishedBoardGeneration, boardsAmount, boardSize, timeElapsed / 1000.0, fullOutputpath, fullLogsPath));
+
+            OpenFileExplorer(fullOutputpath);
+            Console.ReadKey();
+        }
+
+        private static void OpenFileExplorer(string path)
+        {
+            try
+            {
+                Process.Start("explorer.exe", path);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(Messages.ErrorOpeningExplorer);
+            }
+        }
+
+        private static void ConfigureLogger()
+        {
+            if (!Directory.Exists(LogsDirPath))
+            {
+                Directory.CreateDirectory(LogsDirPath);
+            }
+
+            var logFile = Path.Combine(LogsDirPath, string.Format(LogsFileName, DateTime.UtcNow));
+
+            var seriLog = new LoggerConfiguration()
+                .WriteTo.File(logFile)
+                .MinimumLevel.Information()
+                .CreateLogger();
+
+            var loggerFactory = new LoggerFactory().AddSerilog(seriLog);
+            _logger = loggerFactory.CreateLogger<ArukoneService>();
         }
     }
 }
