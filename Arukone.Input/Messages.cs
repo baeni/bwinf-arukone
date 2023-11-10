7f5e7ea0b401aff011c24fafdf267eebb729f373
Benjamin Saalfeld <benjamin.saalfeld@adesso.de>
2023-11-10 11:15:10 +0100

    Add static Messages class to keep messages centralized

---
+++ Arukone.Input/Messages.cs
@@ -0,0 +1,49 @@
+﻿namespace Arukone.Input
+{
+    internal static class Messages
+    {
+        internal const string SpecifyBoardSize = """
+
+                  `.                      `..                                  
+                 `. ..                    `..                                  
+                `.  `..    `. `...`..  `..`..  `..   `..    `.. `..     `..    
+               `..   `..    `..   `..  `..`.. `..  `..  `..  `..  `.. `.   `.. 
+              `...... `..   `..   `..  `..`.`..   `..    `.. `..  `..`..... `..
+             `..       `..  `..   `..  `..`.. `..  `..  `..  `..  `..`.        
+            `..         `..`...     `..`..`..  `..   `..    `...  `..  `....   
+            
+            Bestimme die Größe des Spielfeldes (>= {0}): 
+            """;
+
+        internal const string StartedBoardGeneration = """
+            
+                  `.                      `..                                  
+                 `. ..                    `..                                  
+                `.  `..    `. `...`..  `..`..  `..   `..    `.. `..     `..    
+               `..   `..    `..   `..  `..`.. `..  `..  `..  `..  `.. `.   `.. 
+              `...... `..   `..   `..  `..`.`..   `..    `.. `..  `..`..... `..
+             `..       `..  `..   `..  `..`.. `..  `..  `..  `..  `..`.        
+            `..         `..`...     `..`..`..  `..   `..    `...  `..  `....   
+            
+            {0} Spielfelder in der Größe {1}*{1} werden generiert...
+            Dieser Vorgang kann einen Augenblick dauern.
+
+            """;
+
+        internal const string FinishedBoardGeneration = """
+            
+                  `.                      `..                                  
+                 `. ..                    `..                                  
+                `.  `..    `. `...`..  `..`..  `..   `..    `.. `..     `..    
+               `..   `..    `..   `..  `..`.. `..  `..  `..  `..  `.. `.   `.. 
+              `...... `..   `..   `..  `..`.`..   `..    `.. `..  `..`..... `..
+             `..       `..  `..   `..  `..`.. `..  `..  `..  `..  `..`.        
+            `..         `..`...     `..`..`..  `..   `..    `...  `..  `....   
+            
+            Es wurden {0} Spielfelder mit einer Größe von {1}*{1} erstellt.
+            Die Ergebnisse und Logs kannst du unter "{2}" bzw. "{3}" einsehen.
+
+            | Der Vorgang hat {4}s gedauert.
+            """;
+    }
+}
